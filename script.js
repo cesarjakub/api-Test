3a0d6571ba97b2a947259586cf554cc2bcd3a0b0
cesarjakub <list>
2023-03-30 09:05:02 +0200

    feat: test api

---
+++ script.js
@@ -0,0 +1,108 @@
+class Liked{
+    constructor(){
+        this.poleLiked = [];
+    }
+
+    addFact(text){
+        this.poleLiked.push(text);
+    }
+
+    printLiked(){
+        return this.poleLiked + " ";
+    }
+
+}
+
+class Disliked{
+    constructor(){
+        this.poleDisLiked = [];
+    }
+
+    addFact(text){
+        this.poleDisLiked.push(text);
+    }
+
+    printDisLiked(){
+        return this.poleDisLiked  + " ";
+    }
+}
+$(document).ready(() => {
+
+    let ids = ["bitcoin","ethereum","tether","cardano","dogecoin","solana","spacevikings","stellar","fantom"];
+
+    function dogCard(img, name,czk,fol){
+        let text = `
+        <div class="cards" id="cards">
+            <div class="card" style="width: 18rem;">
+            <img src="${img}" class="card-img-top" alt="img">
+            <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+             <p class="card-text">CZK: ${czk} Kč</p>
+             <p class="card-text">Twitter followers: ${fol}</p>
+            </div>
+        </div>
+      </div>`;
+        return text;
+    }
+
+
+
+    function getCoinID(id){
+        $.ajax({
+            url: `https://api.coingecko.com/api/v3/coins/${id}`,
+            type: "GET",
+            success: function (result) {
+              console.log(result);
+              $(".coinFact").append(dogCard(result.image.large, result.id, result.market_data.current_price.czk ,result.community_data.twitter_followers));
+            },
+            error: function (error) {
+              console.log("Error");
+            },
+        });
+    }
+
+    let id;
+    $(".newFact").click(function() {
+        id = Math.floor((Math.random() * ids.length));
+        console.log(ids[id]);
+        getCoinID(ids[id]);
+        $(".newFact").css("display","none");
+        
+    });
+
+    $(".newFactCard").click(function() {
+        $("#coinFact").empty();
+        id = Math.floor((Math.random() * ids.length));
+        console.log(ids[id]);
+        getCoinID(ids[id]);
+    });
+    
+    let a = new Liked();
+    let b = new Disliked();
+
+    $("#like").click(function() {
+        a.addFact(ids[id]);
+        $(".anim").animate({
+            left: '100px',
+            right: '100px'
+        })
+        console.log("liked");
+    });
+    $("#dislike").click(function() {
+        b.addFact(ids[id]);
+        $("#coinFact").animate({
+            left: '-100px',
+            right: '-100px'
+        });
+        console.log("disliked");
+    });
+    
+    $(".likedBtn").click(function() {
+        $(".liked").append(a.printLiked());
+    });
+    $(".dislikedBtn").click(function() {
+        $(".disliked").append(b.printDisLiked());
+    });
+    
+
+});
